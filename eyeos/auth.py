9898149a990c47c6d0601f5b7aeaf253e63951be
Vishesh Handa <me@vhanda.in>
2016-06-13 15:54:37 +0200

    Open365 Auth: Load proxy from env variable
    Commit be110e232044f71cc3a252ca81775f33f38dd3c3 from docker-seafile

--- eyeos/auth.py
+++ eyeos/auth.py
@@ -61,7 +61,8 @@ class EyeosCardAuthBackend(object):
     def _validate_card(self, card, signature):
         headers = {"Content-type": "application/json", "Accept": "*/*", "card": card, "signature": signature}
         try:
-            conn = httplib.HTTPSConnection("proxy.service.consul", context=ssl._create_unverified_context())
+            proxy_service_consul = os.environ.get("PROXY_DOCKER_NAME") or "proxy.service.consul"
+            conn = httplib.HTTPSConnection(proxy_service_consul, context=ssl._create_unverified_context())
             conn.request("POST", "/login/v1/methods/checkCard/", None, headers)
             res = conn.getresponse()
             response_status = res.status
