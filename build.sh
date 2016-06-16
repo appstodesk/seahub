126aa0f708dd54a15fca01b611d8222c873c5b97
Franky Armengol <francesc.armengol@eyeos.com>
2016-06-16 17:09:10 +0200

    Extract build command to make development easier.

---
+++ build.sh
@@ -0,0 +1,10 @@
+set -x
+set -u
+set -o pipefail
+
+cd /var/lib/seafile/scripts/seahub
+export PYTHONPATH=$PWD/../seafile/lib64/python2.6/site-packages:$PWD/thirdpart:$PWD
+export PATH=$PATH:thirdpart/Django-1.5.12-py2.6.egg/django/bin
+export CCNET_CONF_DIR=$PWD/../../conf
+export SEAFILE_CONF_DIR="$CCNET_CONF_DIR"
+make dist
